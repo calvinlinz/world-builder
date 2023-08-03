e798e78405d2e8544c7e2cdd6db3694cb7e203c6
Calvin Li <callvinlinz@gmail.com>
2023-08-03 18:19:14 +1200

    successfully fetched data from java API to front end

--- world-builder/src/App.js
+++ world-builder/src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [worldData, setWorldData] = useState([]);
+
+  useEffect(() => {
+    // Fetch data from the API
+    fetch("http://localhost:8080/world")
+      .then((response) => response.json())
+      .then((data) => setWorldData(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul>
+        {Array.isArray(worldData) ? (
+          worldData.map((item) => <li key={item.id}>{item.name}</li>)
+        ) : (
+          <li key={worldData.id}>{worldData.name}</li>
+        )}
+      </ul>
     </div>
   );
 }
