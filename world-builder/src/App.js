a11fda7f52541a6965dc616e46107df79fc0bc4a
Annie Cho <choanni@ecs.vuw.ac.nz>
2023-08-09 15:02:51 +1200

    Added basic implementation of homepage

--- world-builder/src/App.js
+++ world-builder/src/App.js
@@ -5,6 +5,7 @@ import Grid from './Grid';
 
 function App() {
   const [worldData, setWorldData] = useState([]);
+  const [gameStarted, setGameStarted] = useState(false); // Step 2
 
   useEffect(() => {
     // Fetch data from the API
@@ -14,17 +15,32 @@ function App() {
       .catch((error) => console.error("Error fetching data:", error));
   }, []);
 
+  const startGame = () => {
+    setGameStarted(true); // Step 3
+  };
+
   return (
     <div className="App">
-      <ul>
-        {Array.isArray(worldData) ? (
-          worldData.map((item) => <li key={item.id}>{item.name}</li>)
-        ) : (
-          <li key={worldData.id}>{worldData.name}</li>
-        )}
-      </ul>
-
-      <Grid />
+      {gameStarted ? ( // Step 4
+        <>
+          <ul>
+            {Array.isArray(worldData) ? (
+              worldData.map((item) => <li key={item.id}>{item.name}</li>)
+            ) : (
+              <li key={worldData.id}>{worldData.name}</li>
+            )}
+          </ul>
+          <Grid />
+        </>
+      ) : (
+        <div className="homepage">
+          <header className="hero">
+              <h1>Dungeons & Dragons Map Generator</h1>
+              <p><b>Embark on an exciting journey as a Dungeon Master.</b></p>
+              <button onClick={startGame}>Generate Map</button>
+          </header>
+        </div>
+      )}
     </div>
   );
 }
