c3c7d5eecc8de00378c9ce5fafa92b1240fbee27
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2023-08-09 13:30:45 +1200

    fixing formatting

--- API/src/main/java/com/API/service/QaudrantBuilder.java
+++ API/src/main/java/com/API/service/QaudrantBuilder.java
@@ -8,76 +8,73 @@ import com.API.model.RoomManager;
 
 public class QaudrantBuilder {
 
+    public static int[][] getQaudrant(int arrayS, int rooms) {
 
-    public static int [][] getQaudrant(int arrayS, int rooms){
+        // Assuming you have a 20x20 array filled with 0s and a 1 at one position
 
-         // Assuming you have a 20x20 array filled with 0s and a 1 at one position
-        
         RoomManager roomM = new RoomManager();
         NaturalFeatureManager natM = new NaturalFeatureManager();
         Random random = new Random();
         int roomCount = rooms;
         int arraySize = arrayS;
-        int count =0;
+        int count = 0;
         int[][] array = new int[arraySize][arraySize];
 
-        while(count < roomCount){
-        int randomRow = random.nextInt(arraySize);
-        int randomCol = random.nextInt(arraySize);
+        while (count < roomCount) {
+            int randomRow = random.nextInt(arraySize);
+            int randomCol = random.nextInt(arraySize);
 
-        int duel = random.nextInt(11);
-        int id =0;
-        Element currentElement = null;
+            int duel = random.nextInt(11);
+            int id = 0;
+            Element currentElement = null;
 
-        if(duel < 7 ){
-            id =  roomM.getRandomRoom().getId();
-            currentElement = roomM.getRoom(String.valueOf(id));
+            if (duel < 7) {
+                id = roomM.getRandomRoom().getId();
+                currentElement = roomM.getRoom(String.valueOf(id));
 
-        }
-        else {
-            id =  natM.getRandomFeature().getId();
-            currentElement = natM.getNaturalFeature(String.valueOf(id));
+            } else {
+                id = natM.getRandomFeature().getId();
+                currentElement = natM.getNaturalFeature(String.valueOf(id));
 
-        }
-
-       
-        int num = id;
-        int topLeftRow = Math.max(randomRow, 0);
-        int topLeftCol = Math.max(randomCol, 0);
-        int bottomRightRow = randomRow + currentElement.getHeight();
-        int bottomRightCol = randomCol + currentElement.getWidth();
-
-        if(checkAval(array,topLeftRow,bottomRightRow,topLeftCol,bottomRightCol)){
-
-        for (int i = topLeftRow; i < bottomRightRow; i++) {
-            for (int j = topLeftCol; j < bottomRightCol; j++) {
-                array[i][j] = num;
             }
-        }
 
-        count++;
-      
-        }    
+            int num = id;
+            int topLeftRow = Math.max(randomRow, 0);
+            int topLeftCol = Math.max(randomCol, 0);
+            int bottomRightRow = randomRow + currentElement.getHeight();
+            int bottomRightCol = randomCol + currentElement.getWidth();
+
+            if (checkAval(array, topLeftRow, bottomRightRow, topLeftCol, bottomRightCol)) {
+
+                for (int i = topLeftRow; i < bottomRightRow; i++) {
+                    for (int j = topLeftCol; j < bottomRightCol; j++) {
+                        array[i][j] = num;
+                    }
+                }
+
+                count++;
+
+            }
         }
         printMap(array);
 
         return array;
     }
 
-    public static boolean checkAval(int[][] array, int tlr, int brr,int tlc, int brc){
-        if(brc>array.length || brr>array.length){
+    public static boolean checkAval(int[][] array, int tlr, int brr, int tlc, int brc) {
+        if (brc > array.length || brr > array.length) {
             return false;
         }
         brr = (brr != array.length) ? brr + 1 : brr;
         brc = (brc != array.length) ? brc + 1 : brc;
         tlr = (tlr != 0) ? tlr - 1 : tlr;
-        tlc = (tlc != 0) ? tlc - 1 : tlc;   
-       
+        tlc = (tlc != 0) ? tlc - 1 : tlc;
+
         for (int i = tlr; i < brr; i++) {
             for (int j = tlc; j < brc; j++) {
 
-                if(array[i][j] != 0){
-                    
+                if (array[i][j] != 0) {
+
                     return false;
                 }
             }
@@ -85,38 +82,25 @@ public class QaudrantBuilder {
         return true;
     }
 
-    public static void printMap(int [][] array){
+    public static void printMap(int[][] array) {
         // Print the resulting array to see the 4x4 square
         String red = "\u001B[31m";
-        String green ="\u001B[32m";
+        String green = "\u001B[32m";
         String resetColor = "\u001B[0m";
-       for (int i = 0; i < array.length; i++) {
-           for (int j = 0; j < array[i].length; j++) {
-               if(array[i][j] >=20 ){
-               System.out.print(green + array[i][j] + " " + resetColor);
-               }
-               else if(array[i][j] != 0){
-                   System.out.print(red + array[i][j] + " " + resetColor);
-               }
-               else{
-                   System.out.print(array[i][j] + " ");
-               }
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (array[i][j] >= 20) {
+                    System.out.print(green + array[i][j] + " " + resetColor);
+                } else if (array[i][j] != 0) {
+                    System.out.print(red + array[i][j] + " " + resetColor);
+                } else {
+                    System.out.print(array[i][j] + " ");
+                }
 
-           }
-           System.out.println();
-       }
+            }
+            System.out.println();
+        }
 
-   }
+    }
 
-
-
-
-
-
-
-
-
-
-
-    
 }
