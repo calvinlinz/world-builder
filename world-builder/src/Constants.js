683ce5e0f722fa3c8f08952d09e491b1df8741bc
wipateella <wipateella@ecs.vuw.ac.nz>
2023-08-09 13:07:59 +1200

    Upload all of the images to the Constants.js file

---
+++ world-builder/src/Constants.js
@@ -0,0 +1,93 @@
+// Camp images:
+import tent_1x2 from './assets/camp/1x2_tent.png';
+import tent_2x2_1 from './assets/camp/2x2_tent_1.png';
+import tent_2x2_2 from './assets/camp/2x2_tent_2.png';
+import bedroll from './assets/camp/bedroll.png';
+import campfire from './assets/camp/campfire.png';
+import fire from './assets/camp/fire.png';
+import firewood from './assets/camp/firewood.png';
+
+// Cave images:
+import corner_long from './assets/caves/corner_long.png';
+import corner from './assets/caves/corner.png';
+import floor_light from './assets/caves/floor_light.jpg';
+import floor_dark from './assets/caves/floor_dark.jpg';
+import straight from './assets/caves/straight.png';
+
+// Forest images:
+import bush1 from './assets/forest/bush1.png';
+import bush2 from './assets/forest/bush2.png';
+import bush3 from './assets/forest/bush3.png';
+import duff_lrg from './assets/forest/duff_lrg.png';
+import duff_med from './assets/forest/duff_med.png';
+import duff_small from './assets/forest/duff_small.png';
+import fallen_tree from './assets/forest/fallen_tree.png';
+import grass_texture_long from './assets/forest/grass_texture_long.png';
+import grass_texture_rock from './assets/forest/grass_texture_rock.png';
+import grass_texture_short from './assets/forest/grass_texture_short.png';
+import grass_texture from './assets/forest/grass_texture.png';
+import grass1 from './assets/forest/grass1.jpg';
+import grass2 from './assets/forest/grass2.jpg';
+import grass3 from './assets/forest/grass3.jpg';
+import leaf_green from './assets/forest/leaf_green.png';
+import leaf_red1 from './assets/forest/leaf_red1.png';
+import leaf_red2 from './assets/forest/leaf_red2.png';
+import orange_bush1 from './assets/forest/orange_bush1.png';
+import orange_bush2 from './assets/forest/orange_bush2.png';
+import orange_bush3 from './assets/forest/orange_bush3.png';
+import orange_bush4 from './assets/forest/orange_bush4.png';
+import tree1 from './assets/forest/tree1.png';
+import tree2 from './assets/forest/tree2.png';
+import tree3 from './assets/forest/tree3.png';
+import tree4 from './assets/forest/tree4.png';
+import tree5 from './assets/forest/tree5.png';
+import tree6 from './assets/forest/tree6.png';
+
+// Path images:
+import path_light from './assets/paths/path_light.jpg';
+import path_dark from './assets/paths/path_dark.jpg';
+
+// Rock images:
+import clstr_rock1 from './assets/rocks/clstr_rock1.png';
+import clstr_rock2 from './assets/rocks/clstr_rock2.png';
+import clstr_rock3 from './assets/rocks/clstr_rock3.png';
+import lrg_rock1 from './assets/rocks/lrg_rock1.png';
+import lrg_rock2 from './assets/rocks/lrg_rock2.png';
+import lrg_rock3 from './assets/rocks/lrg_rock3.png';
+import lrg_rock4 from './assets/rocks/lrg_rock4.png';
+import sml_rock from './assets/rocks/sml_rock1.png';
+
+// Village images:
+import building_2x2 from './assets/village/2x2_building.png';
+import roof_blue_2x2 from './assets/village/2x2_roof_blue.png';
+import roof_red_2x2 from './assets/village/2x2_roof_red.png';
+import building_2x3 from './assets/village/2x3_building.png';
+import roof_blue_2x3 from './assets/village/2x3_roof_blue.png';
+import roof_red_2x3 from './assets/village/2x3_roof_red.png';
+import building_3x3 from './assets/village/3x3_building.png';
+import roof_blue_3x3 from './assets/village/3x3_roof_blue.png';
+import roof_red_3x3 from './assets/village/3x3_roof_red.png';
+import building_4x4 from './assets/village/4x4_building.png';
+import roof_blue_4x4 from './assets/village/4x4_roof_blue.png';
+import roof_red_4x4 from './assets/village/4x4_roof_red.png';
+import building_4x6 from './assets/village/4x6_building.png';
+import roof_blue_4x6 from './assets/village/4x6_roof_blue.png';
+import roof_red_4x6 from './assets/village/4x6_roof_red.png';
+import building_5x5 from './assets/village/5x5_building.png';
+import roof_blue_5x5 from './assets/village/5x5_roof_blue.png';
+import roof_red_5x5 from './assets/village/5x5_roof_red.png';
+import building_6x3 from './assets/village/6x3_building.png';
+import roof_blue_6x3 from './assets/village/6x3_roof_blue.png';
+import roof_red_6x3 from './assets/village/6x3_roof_red.png';
+import building_6x8 from './assets/village/6x8_building.png';
+import roof_blue_6x8 from './assets/village/6x8_roof_blue.png';
+import roof_red_6x8 from './assets/village/6x8_roof_red.png';
+import building_7x8 from './assets/village/7x8_building.png';
+import roof_blue_7x8 from './assets/village/7x8_roof_blue.png';
+import roof_red_7x8 from './assets/village/7x8_roof_red.png';
+
+// Water images:
+import bridge from './assets/water/bridge.jpg';
+import water_rock from './assets/water/water_rock.png';
+import water from './assets/water/water.jpg';
+import waterfall from './assets/water/waterfall.png';
