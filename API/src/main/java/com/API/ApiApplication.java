3aff0b654f029050ec9b5303143e85dfeadba8cf
Amir Mandalawi <mandalamir@ecs.vuw.ac.nz>
2023-08-10 13:43:33 +1200

    importing the MapBuilder in the ApiApplication class

--- API/src/main/java/com/API/ApiApplication.java
+++ API/src/main/java/com/API/ApiApplication.java
@@ -3,6 +3,8 @@ package com.API;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.API.service.MapBuilder;
+
 @SpringBootApplication
 public class ApiApplication {
 
