36171acd34e289dfc753a48ca9ee5030eca67366
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2023-09-06 10:47:08 +1200

    adding node class for path finding

---
+++ API/src/main/java/com/API/model/Node.java
@@ -0,0 +1,23 @@
+package com.API.model;
+
+public class Node {
+    int x, y;
+
+    public Node(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Node node = (Node) obj;
+        return x == node.x && y == node.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return 31 * x + y;
+    }
+}
