38794963367d78dc8edd04b71a21b3e76b5f269e
Milan Kriletich <kriletmila@ecs.vuw.ac.nz>
2023-09-06 14:21:43 +1200

    creating astar method which links to rooms

--- API/src/main/java/com/API/service/AStarPathFinding.java
+++ API/src/main/java/com/API/service/AStarPathFinding.java
@@ -1,5 +1,12 @@
 package com.API.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.PriorityQueue;
+
 import com.API.model.Node;
 
 public class AStarPathFinding {
@@ -12,6 +19,61 @@ public class AStarPathFinding {
     // Heuristic function to estimate the distance between two nodes (Manhattan distance)
     static int heuristic(Node a, Node b) {
         return Math.abs(a.x - b.x) + Math.abs(a.y - b.y);
+    }
+
+    // A* algorithm to find the shortest path between two nodes on a grid
+    static List<Node> astar(Node start, Node end, int[][] grid) {
+        int rows = grid.length;
+        int cols = grid[0].length;
+
+        // Priority queue to keep track of nodes with the lowest estimated total cost
+        PriorityQueue<Node> openSet = new PriorityQueue<>(Comparator.comparingInt((Node node) -> heuristic(node, end)));
+        openSet.add(start);
+
+        // Maps to store the optimal path and cost to reach each node
+        Map<Node, Node> cameFrom = new HashMap<>();
+        Map<Node, Integer> gScore = new HashMap<>();
+        gScore.put(start, 0);
+
+        while (!openSet.isEmpty()) {
+            Node current = openSet.poll();
+
+            // If the current node is the end node, reconstruct and return the path
+            if (current.equals(end)) {
+                List<Node> path = reconstructPath(cameFrom, current);
+                return path;
+            }
+
+            // Explore neighbors of the current node
+            for (int i = 0; i < 4; i++) {
+                int newX = current.x + dx[i];
+                int newY = current.y + dy[i];
+                Node neighbor = new Node(newX, newY);
+
+                // Check if the neighbor is within the grid boundaries and is passable
+                if (newX >= 0 && newX < rows && newY >= 0 && newY < cols && (grid[newX][newY] == 0 || grid[newX][newY] == 40) ) {
+                    int tentativeGScore = gScore.get(current) + 1;
+                    if (!gScore.containsKey(neighbor) || tentativeGScore < gScore.get(neighbor)) {
+                        cameFrom.put(neighbor, current);
+                        gScore.put(neighbor, tentativeGScore);
+                        openSet.add(neighbor);
+                    }
+                }
+            }
+        }
+
+        // If no path is found, return null
+        return null;
+    }
+
+    // Reconstruct the path from the destination node to the start node
+    static List<Node> reconstructPath(Map<Node, Node> cameFrom, Node current) {
+        List<Node> path = new ArrayList<>();
+        while (current != null) {
+            path.add(current);
+            current = cameFrom.get(current);
+        }
+        return path;
     }
     
 }
