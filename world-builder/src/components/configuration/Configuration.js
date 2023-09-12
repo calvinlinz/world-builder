836b8df6d3ebce49346fd42df0f197d84d5a6df6
Calvin Li <callvinlinz@gmail.com>
2023-09-12 13:21:13 +1200

    Create dropdown menu to allow for png and json downloading formats

--- world-builder/src/components/configuration/Configuration.js
+++ world-builder/src/components/configuration/Configuration.js
@@ -4,66 +4,100 @@ import Slider from "@mui/material/Slider";
 import FormGroup from "@mui/material/FormGroup";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import Button from "@mui/material/Button";
+import { Button, Menu, MenuItem } from "@mui/material";
 import "../../Grid.css";
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 
-const ConfigDropdown = ({ opacityToggle, setScaleFactorImages }) => {
+const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
   const [showInputs, setShowInputs] = useState(false);
   const [numberOfRooms, setNumberOfRooms] = useState(5);
   const [showFog, setShowFog] = useState(true);
   const [addRemoveFog, setAddRemoveFog] = useState(false);
-  
+  const [downloadDropdown, setDownloadDropdown] = useState(false);
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleDropdownOpen = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleDropdownClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleOptionSelect = (option) => {
+    setSelectedOption(option);
+    handleDropdownClose();
+    if (option == "png") {
+      downloadPNG();
+    } else if (option == "json") {
+      downloadJSON();
+    }
+  };
   const marks = [
     { value: 1, label: "1" },
     { value: 5, label: "5" },
     { value: 10, label: "10" },
   ];
-  
+
   const [isChanged, setIsChanged] = useState(false);
 
   const handleMenuClick = () => {
     setShowInputs(!showInputs);
   };
 
-  const handleDownload = () => {
+  const downloadJSON = () => {
+    if (worldData) {
+      const jsonData = JSON.stringify(worldData);
+      const blob = new Blob([jsonData], { type: "application/json" });
+      const url = window.URL.createObjectURL(blob);
+      const date = new Date().toISOString();
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = "worldData-" + date+ ".json";
+      a.click();
+      window.URL.revokeObjectURL(url);
+    }
+  };
+
+  const downloadPNG = () => {
     setShowInputs(!showInputs);
     setTimeout(() => {
-      const targetElement = document.documentElement;      
+      const targetElement = document.documentElement;
       html2canvas(targetElement, {
-        width: window.innerWidth,    // Set the width of the screenshot
-        height: window.innerHeight,  // Set the height of the screenshot
-        x: 0,            // X-coordinate of the top-left corner of the screenshot
-        y: 0,            // Y-coordinate of the top-left corner of the screenshot
+        width: window.innerWidth, // Set the width of the screenshot
+        height: window.innerHeight, // Set the height of the screenshot
+        x: 0, // X-coordinate of the top-left corner of the screenshot
+        y: 0, // Y-coordinate of the top-left corner of the screenshot
       }).then((canvas) => {
         // Convert the canvas content to a data URL (PNG image)
-        const dataURL = canvas.toDataURL('image/png');
+        const dataURL = canvas.toDataURL("image/png");
         // Create a download link
-        const downloadLink = document.createElement('a');
+        const downloadLink = document.createElement("a");
         downloadLink.href = dataURL;
-        downloadLink.download = 'screenshot.png'; // Set the filename
+        downloadLink.download = "screenshot.png"; // Set the filename
 
         // Trigger a click event on the download link to initiate the download
         downloadLink.click();
       });
     }, 0);
-  }
-
+  };
 
   const handleGenerate = () => {
     console.log("Logic for generating a new map TBC");
-  }
-  
+  };
+
   return (
     <div className="body">
       <div className="dropDown">
         <div id="hamburger" onClick={handleMenuClick}>
-          <div className={`container ${showInputs ? 'change' : ''}`}>
+          <div className={`container ${showInputs ? "change" : ""}`}>
             <div className="bar1"></div>
             <div className="bar2"></div>
             <div className="bar3"></div>
           </div>
-      </div>
+        </div>
       </div>
       {showInputs && (
         <div className="content">
@@ -77,7 +111,7 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages }) => {
                 min={3}
                 max={7}
                 marks={marks}
-                onChange={(e)=>setScaleFactorImages(e.target.value)}
+                onChange={(e) => setScaleFactorImages(e.target.value)}
               />
             </div>
           </div>
@@ -90,50 +124,70 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages }) => {
                 valueLabelDisplay="auto"
                 min={1}
                 max={10}
-                onChange={(e)=>setNumberOfRooms(e.target.value)}
+                onChange={(e) => setNumberOfRooms(e.target.value)}
                 marks={marks}
               />
             </div>
           </div>
           <div className="formGroup">
-          <FormGroup>
-            <FormControlLabel
-              control={<Checkbox defaultChecked />}
-              onChange={()=>setShowFog(!showFog)}
-              label="SHOW FOG"
-            />
-            <FormControlLabel
-              control={<Checkbox/>}
-              onChange={()=>setAddRemoveFog(!addRemoveFog)}
-              label="ADD/REMOVE FOG"
-            />
-            <FormControlLabel
-              control={<Checkbox/>}
-              onChange={opacityToggle}
-              label="ADD/REMOVE ROOFS"
-            />
-          </FormGroup>
+            <FormGroup>
+              <FormControlLabel
+                control={<Checkbox defaultChecked />}
+                onChange={() => setShowFog(!showFog)}
+                label="SHOW FOG"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                onChange={() => setAddRemoveFog(!addRemoveFog)}
+                label="ADD/REMOVE FOG"
+              />
+              <FormControlLabel
+                control={<Checkbox />}
+                onChange={opacityToggle}
+                label="ADD/REMOVE ROOFS"
+              />
+            </FormGroup>
           </div>
           <div className="button-container">
             <div className="button">
-              <Button variant="outlined" onClick={handleDownload} style={{
-                  color: '#000000', 
-                  borderColor: '#000000', 
-                  borderWidth: '1px', 
-                }}>
-                  DOWNLOAD</Button>
+              <Button
+                variant="outlined"
+                onClick={handleDropdownOpen}
+                style={{
+                  color: "#000000",
+                  borderColor: "#000000",
+                  borderWidth: "1px",
+                }}
+              >
+                Save
+              </Button>
+              <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleDropdownClose}
+              >
+                <MenuItem onClick={() => handleOptionSelect("png")}>
+                  PNG
+                </MenuItem>
+                <MenuItem onClick={() => handleOptionSelect("json")}>
+                  JSON
+                </MenuItem>
+              </Menu>
             </div>
             <div className="button">
-              <Button variant="outlined" onClick={handleGenerate} 
+              <Button
+                variant="outlined"
+                onClick={handleGenerate}
                 style={{
-                  color: '#000000', 
-                  borderColor: '#000000', 
-                  borderWidth: '1px', 
-                }
-                }>GENERATE</Button>
+                  color: "#000000",
+                  borderColor: "#000000",
+                  borderWidth: "1px",
+                }}
+              >
+                GENERATE
+              </Button>
             </div>
           </div>
-          
         </div>
       )}
     </div>
