b18c3536061b0786390d9160378e84810f54cf6f
Calvin Li <callvinlinz@gmail.com>
2023-09-12 13:48:10 +1200

    Fixed dropdown button animation

--- world-builder/src/components/configuration/Configuration.js
+++ world-builder/src/components/configuration/Configuration.js
@@ -4,9 +4,9 @@ import Slider from "@mui/material/Slider";
 import FormGroup from "@mui/material/FormGroup";
 import Checkbox from "@mui/material/Checkbox";
 import FormControlLabel from "@mui/material/FormControlLabel";
-import {Menu, Button, MenuItem, FormControl, Select} from "@mui/material";
+import { Menu, Button, MenuItem, FormControl, Select } from "@mui/material";
 import "../../Grid.css";
-import html2canvas from 'html2canvas';
+import html2canvas from "html2canvas";
 import MonstersOverlay from "./MonstersOverlay";
 
 const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
@@ -36,7 +36,6 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
     }
   };
 
-
   const downloadJSON = () => {
     if (worldData) {
       const jsonData = JSON.stringify(worldData);
@@ -54,29 +53,29 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
   const downloadPNG = () => {
     setShowInputs(!showInputs);
     setTimeout(() => {
-      const targetElement = document.documentElement;      
-      html2canvas(targetElement, {   
+      const targetElement = document.documentElement;
+      html2canvas(targetElement, {
         width: window.innerWidth, // Set the width of the screenshot
         height: window.innerHeight, // Set the height of the screenshot
         x: 0, // X-coordinate of the top-left corner of the screenshot
         y: 0, // Y-coordinate of the top-left corner of the screenshot
       }).then((canvas) => {
         // Convert the canvas content to a data URL (PNG image)
-        const dataURL = canvas.toDataURL('image/png');
+        const dataURL = canvas.toDataURL("image/png");
         // Create a download link
-        const downloadLink = document.createElement('a');
+        const downloadLink = document.createElement("a");
         downloadLink.href = dataURL;
-        downloadLink.download = 'screenshot.png'; // Set the filename
+        downloadLink.download = "screenshot.png"; // Set the filename
         downloadLink.click();
       });
     }, 0);
-  }
+  };
   const marks = [
     { value: 1, label: "1" },
     { value: 5, label: "5" },
     { value: 10, label: "10" },
   ];
-  
+
   const [isChanged, setIsChanged] = useState(false);
 
   const handleMenuClick = () => {
@@ -89,108 +88,90 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
 
   let contentToRender;
 
-  if (selectedMonsterOption === 'none') {
+  if (selectedMonsterOption === "none") {
     contentToRender = <div>None selected</div>;
-  } else if (selectedMonsterOption === 'option2') {
-    contentToRender = <MonstersOverlay className="monsterContent" monsterName={"Fairy Test"} monsterRank={"Fairy Rank"}/>;
-  } else if (selectedMonsterOption === 'option3') {
+  } else if (selectedMonsterOption === "option2") {
+    contentToRender = (
+      <MonstersOverlay
+        className="monsterContent"
+        monsterName={"Fairy Test"}
+        monsterRank={"Fairy Rank"}
+      />
+    );
+  } else if (selectedMonsterOption === "option3") {
     contentToRender = <div>Option 3 selected</div>;
   }
 
-  const handleDownload = () => {
-    setShowInputs(!showInputs);
-    setTimeout(() => {
-      const targetElement = document.documentElement;      
-      html2canvas(targetElement, {
-        width: window.innerWidth,    // Set the width of the screenshot
-        height: window.innerHeight,  // Set the height of the screenshot
-        x: 0,            // X-coordinate of the top-left corner of the screenshot
-        y: 0,            // Y-coordinate of the top-left corner of the screenshot
-      }).then((canvas) => {
-        // Convert the canvas content to a data URL (PNG image)
-        const dataURL = canvas.toDataURL('image/png');
-        // Create a download link
-        const downloadLink = document.createElement('a');
-        downloadLink.href = dataURL;
-        downloadLink.download = 'screenshot.png'; // Set the filename
-
-        // Trigger a click event on the download link to initiate the download
-        downloadLink.click();
-      });
-    }, 0);
-  }
-
-
   const handleGenerate = () => {
     console.log("Logic for generating a new map TBC");
-  }
-  
+  };
+
   return (
-    <div>
-      <div className="body">
-        <div className="dropDown">
-          <div id="hamburger" onClick={handleMenuClick}>
-            <div className={`container ${showInputs ? 'change' : ''}`}>
-              <div className="bar1"></div>
-              <div className="bar2"></div>
-              <div className="bar3"></div>
-            </div>
+    <div className="body">
+      <div className="dropDown">
+        <div id="hamburger" onClick={handleMenuClick}>
+          <div className={`container ${showInputs ? "change" : ""}`}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
         </div>
-        </div>
-        {showInputs && (
-          <div className="content">
-            <div className="slider-component">
-              <p>GRID ZOOM</p>
-              <div className="slider">
-                <Slider
-                  defaultValue={0.25}
-                  aria-label="Small"
-                  valueLabelDisplay="auto"
-                  min={3}
-                  max={7}
-                  marks={marks}
-                  onChange={(e)=>setScaleFactorImages(e.target.value)}
-                />
-              </div>
+      </div>
+
+      {showInputs && (
+        <div className="content">
+          <div className="slider-component">
+            <p>GRID ZOOM</p>
+            <div className="slider">
+              <Slider
+                defaultValue={0.25}
+                aria-label="Small"
+                valueLabelDisplay="auto"
+                min={3}
+                max={7}
+                marks={marks}
+                onChange={(e) => setScaleFactorImages(e.target.value)}
+              />
             </div>
-            <div className="formGroup">
+          </div>
+          <div className="formGroup">
             <FormGroup>
               <FormControlLabel
                 control={<Checkbox defaultChecked />}
-                onChange={()=>setShowFog(!showFog)}
+                onChange={() => setShowFog(!showFog)}
                 label="SHOW FOG"
               />
               <FormControlLabel
-                control={<Checkbox/>}
-                onChange={()=>setAddRemoveFog(!addRemoveFog)}
+                control={<Checkbox />}
+                onChange={() => setAddRemoveFog(!addRemoveFog)}
                 label="ADD/REMOVE FOG"
               />
               <FormControlLabel
-                control={<Checkbox/>}
+                control={<Checkbox />}
                 onChange={opacityToggle}
                 label="ADD/REMOVE ROOFS"
               />
             </FormGroup>
-            </div>
-            <FormControl>
-              <p>VIEW MONSTER STATS</p> 
-              <Select
-                value={selectedMonsterOption}
-                onChange={handleSelectChange}
-                style={{
-                  color: '#000000', 
-                  borderColor: '#000000', 
-                  borderWidth: '1px', 
-                  margin:'0px 0px 30px 0px',
-                }}
-              >
-                <MenuItem value="none">None</MenuItem>
-                <MenuItem value="option2">Option 2</MenuItem>
-                <MenuItem value="option3">Option 3</MenuItem>
-              </Select>
-            </FormControl>
-            
-            <div className="button-container">
+          </div>
+          <FormControl>
+            <p>VIEW MONSTER STATS</p>
+            <Select
+              value={selectedMonsterOption}
+              onChange={handleSelectChange}
+              style={{
+                color: "#000000",
+                borderColor: "#000000",
+                borderWidth: "1px",
+                margin: "0px 0px 30px 0px",
+              }}
+            >
+              <MenuItem value="none">None</MenuItem>
+              <MenuItem value="option2">Option 2</MenuItem>
+              <MenuItem value="option3">Option 3</MenuItem>
+            </Select>
+          </FormControl>
+
+          <div className="button-container">
             <div className="button">
               <Button
                 variant="outlined"
@@ -218,19 +199,21 @@ const ConfigDropdown = ({ opacityToggle, setScaleFactorImages, worldData }) => {
             </div>
 
             <div className="button">
-                <Button variant="outlined" onClick={handleGenerate} 
-                  style={{
-                    color: '#000000', 
-                    borderColor: '#000000', 
-                    borderWidth: '1px', 
-                  }
-                  }>GENERATE</Button>
-              </div>
+              <Button
+                variant="outlined"
+                onClick={handleGenerate}
+                style={{
+                  color: "#000000",
+                  borderColor: "#000000",
+                  borderWidth: "1px",
+                }}
+              >
+                GENERATE
+              </Button>
             </div>
-            
           </div>
-        )}
-      </div>
+        </div>
+      )}
       {contentToRender}
     </div>
   );
