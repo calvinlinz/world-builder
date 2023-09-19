635f419fb8cef886ac9421bdcd6c8aec7630f9dd
wipateella <wipateella@ecs.vuw.ac.nz>
2023-09-19 18:56:01 +1200

    monster overlay fully implemented

--- world-builder/src/grids/Constants.js
+++ world-builder/src/grids/Constants.js
@@ -106,14 +106,14 @@ import waterfall from '../assets/water/waterfall.png';
 // Monster Images:
 import fairy from '../assets/monsters/fairy.png';
 import shadow from '../assets/monsters/monster_shadow.png';
-import cat from './assets/monsters/cat.png';
-import house1 from './assets/monsters/house1.png';
-import house2 from './assets/monsters/house2.png';
-import house3 from './assets/monsters/house3.png';
-import house4 from './assets/monsters/house4.png';
-import monkey from './assets/monsters/monkey.png';
-import skeleton from './assets/monsters/skeleton.png';
-import snake from './assets/monsters/snake.png';
+import cat from '../assets/monsters/cat.png';
+import house1 from '../assets/monsters/house1.png';
+import house2 from '../assets/monsters/house2.png';
+import house3 from '../assets/monsters/house3.png';
+import house4 from '../assets/monsters/house4.png';
+import monkey from '../assets/monsters/monkey.png';
+import skeleton from '../assets/monsters/skeleton.png';
+import snake from '../assets/monsters/snake.png';
 
 const monsterImages = {
     fairy,
