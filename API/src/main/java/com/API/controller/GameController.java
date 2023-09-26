ebd7ca0ec8c0aa5c058e2b084b3e88e4a8357004
Calvin Li <callvinlinz@gmail.com>
2023-09-27 11:48:16 +1300

    Server side single game functionality implemented

---
+++ API/src/main/java/com/API/controller/GameController.java
@@ -0,0 +1,66 @@
+package com.API.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.API.dto.PersonRequest;
+import com.API.dto.WorldRequest;
+import com.API.model.Person;
+import com.API.service.MapBuilder;
+import com.API.service.MapExporter;
+import com.API.service.MonsterGenerator;
+import com.API.service.PeopleService;
+import com.API.service.WorldService;
+
+@RestController
+@RequestMapping("/game")
+public class GameController {
+
+    private final WorldService worldService;
+
+    private final PeopleService peopleService;
+
+    private int[][] currentMap;
+
+    @Autowired
+    public GameController(WorldService worldService, PeopleService peopleService) {
+        this.worldService = worldService;
+        this.peopleService = peopleService;
+    }
+
+    @PutMapping("/join")
+    public ResponseEntity<int[][]> newPlayer(@RequestBody PersonRequest personRequest) {
+        long id = peopleService.findNextId();
+        Person player = new Person(id,personRequest.getHost());
+        peopleService.newPlayer(player);
+        return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/generate")
+    public ResponseEntity<int[][]> postWorld(@RequestBody WorldRequest worldRequest) {
+        int size = worldRequest.getSize();
+        MapBuilder mb = new MapBuilder(size);
+        mb.createMap();
+        MonsterGenerator mg = new MonsterGenerator(mb);
+        mb.setMap(mg.generateMonsters());
+        MapExporter me = new MapExporter(mb);
+        int[][] jsonContent = me.exportMap();
+        this.currentMap = jsonContent;
+        return ResponseEntity.ok(jsonContent);
+    }
+
+    @GetMapping("/view")
+    public ResponseEntity<int[][]> getWorld(@RequestBody WorldRequest worldRequest) {
+        if (currentMap != null) {
+            return ResponseEntity.ok(currentMap);
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+}
---
+++ API/src/main/java/com/API/dto/PersonRequest.java
@@ -0,0 +1,13 @@
+package com.API.dto;
+
+public class PersonRequest {
+    private boolean host;
+
+    public boolean getHost() {
+        return this.host;
+    }
+    public void setHost(boolean host) {
+        this.host = host;
+    }
+
+}
