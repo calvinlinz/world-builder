7c85de4468b9c8d737bdafee5370b6867e225267
Calvin Li <callvinlinz@gmail.com>
2023-09-28 01:07:05 +1300

    Implemented game id visual

--- world-builder/src/components/playerCount/playerCount.js
+++ world-builder/src/components/playerCount/playerCount.js
@@ -5,16 +5,21 @@ import { useContext } from "react";
 import PersonIcon from "@mui/icons-material/Person";
 
 const PlayerCount = () => {
-    const { currentPlayersInGame } = useContext(WorldDataContext);
+  const { currentPlayersInGame, gameId } = useContext(WorldDataContext);
   return (
     <div className={styles.body}>
       <div className={styles.content}>
         <div className={styles.box}>
-        <div className={styles.icon}>
+          <div className={styles.icon}>
             <PersonIcon />
           </div>
           <div className={styles.count}>
             <p>{currentPlayersInGame}</p>
+          </div>
+        </div>
+        <div className={styles.box2}>
+          <div className={styles.count}>
+            <p>Game ID: {gameId}</p>
           </div>
         </div>
       </div>
