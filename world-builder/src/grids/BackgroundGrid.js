ab5e4608d9dac989ab6ed46cefe9dfb5ac6c3a3d
Amir Mandalawi <mandalamir@ecs.vuw.ac.nz>
2023-09-28 16:06:43 +1300

    importing the world's context

--- world-builder/src/grids/BackgroundGrid.js
+++ world-builder/src/grids/BackgroundGrid.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import { React, useContext } from 'react';
 import './Grid.css';
 import { allImages } from './Constants';
 import { grid2, grid6 } from './TestGrids';
+import { WorldDataContext } from '../context/worldDataContext';
 
-const BackgroundGrid = ({worldData}) => {
+const BackgroundGrid = ({ worldData }) => {
     const grid = worldData;
-
+    const { frameValue } = useContext(WorldDataContext);
     const imageMapping = {
         0: allImages.forestGrass.grass1,
         1: allImages.forestGrass.grass3,
@@ -15,13 +16,13 @@ const BackgroundGrid = ({worldData}) => {
         <div className="grid-container-background">
             {/* Render the grid */}
             {grid.map((row, rowIndex) => (
-            <div key={rowIndex} className="grid-row">
-                {row.map((cell, columnIndex) => (
-                <span key={columnIndex} className="grid-cell">
-                    <img key={columnIndex} className="grid-cell" src={imageMapping[Math.floor(Math.random() * (2))]} alt={`Image ${cell}`} />
-                </span>
-                ))}
-            </div>
+                <div key={rowIndex} className="grid-row">
+                    {row.map((cell, columnIndex) => (
+                        <span key={columnIndex} className="grid-cell">
+                            <img key={columnIndex} className="grid-cell" src={imageMapping[Math.floor(Math.random() * (2))]} alt={`Image ${cell}`} />
+                        </span>
+                    ))}
+                </div>
             ))}
         </div>
     );
