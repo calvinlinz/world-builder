26fd267652f46621b03cae22aac5d0cbd29d3f7c
Annie Cho <choanni@ecs.vuw.ac.nz>
2023-10-03 11:30:52 +1300

    Added extra village and camp codes so forest does not overlap; increased the number of attempts before termination also

--- API/src/main/java/com/API/service/ForestGenerator.java
+++ API/src/main/java/com/API/service/ForestGenerator.java
@@ -34,6 +34,9 @@ public class ForestGenerator {
      */
     private ArrayList<Integer> invalidCodes = new ArrayList<>(List.of(5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23));
 
+    /**
+     * List of invalid tent codes. Some overlap but didn't change.
+     */
     private ArrayList<Integer> tentCodes = new ArrayList<>(List.of(19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30));
 
     /**
